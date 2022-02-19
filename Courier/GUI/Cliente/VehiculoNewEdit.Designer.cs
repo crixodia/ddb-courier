c6a200646db2f1f5b5ed92bd986b3a806109fa6b
Cristian Bastidas <gabrielbastidas5g@outlook.com>
2022-02-19 11:58:04 -0500

    Vehiculo GUI

---
+++ Courier/GUI/Cliente/VehiculoNewEdit.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace Courier.GUI.Vehiculo
+{
+    partial class VehiculoNewEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCi = new System.Windows.Forms.TextBox();
+            this.cmbSuc = new System.Windows.Forms.ComboBox();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtCap = new System.Windows.Forms.TextBox();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCi
+            // 
+            this.txtCi.Location = new System.Drawing.Point(108, 13);
+            this.txtCi.Name = "txtCi";
+            this.txtCi.Size = new System.Drawing.Size(138, 20);
+            this.txtCi.TabIndex = 0;
+            // 
+            // cmbSuc
+            // 
+            this.cmbSuc.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSuc.FormattingEnabled = true;
+            this.cmbSuc.Location = new System.Drawing.Point(108, 39);
+            this.cmbSuc.Name = "cmbSuc";
+            this.cmbSuc.Size = new System.Drawing.Size(138, 21);
+            this.cmbSuc.TabIndex = 1;
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(108, 66);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(138, 20);
+            this.txtPlaca.TabIndex = 2;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(108, 92);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(138, 20);
+            this.txtModel.TabIndex = 3;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(108, 118);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(138, 20);
+            this.txtYear.TabIndex = 4;
+            // 
+            // txtCap
+            // 
+            this.txtCap.Location = new System.Drawing.Point(108, 144);
+            this.txtCap.Name = "txtCap";
+            this.txtCap.Size = new System.Drawing.Size(138, 20);
+            this.txtCap.TabIndex = 5;
+            // 
+            // cmbType
+            // 
+            this.cmbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Items.AddRange(new object[] {
+            "ML",
+            "CL",
+            "CP",
+            "CA"});
+            this.cmbType.Location = new System.Drawing.Point(108, 170);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(138, 21);
+            this.cmbType.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Cédula Empleado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Sucursal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Placa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Modelo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(76, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Año";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Capacidad";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(74, 173);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Tipo";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(91, 207);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 14;
+            this.btnOk.Text = "Añadir";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(172, 207);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // VehiculoNew
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(259, 239);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbType);
+            this.Controls.Add(this.txtCap);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.cmbSuc);
+            this.Controls.Add(this.txtCi);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "VehiculoNew";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Editar Vehículo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCi;
+        private System.Windows.Forms.ComboBox cmbSuc;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.TextBox txtCap;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Courier/GUI/Sucursal/VehiculoGui.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Courier.GUI
+{
+    public partial class VehiculoGui : Form
+    {
+        public VehiculoGui()
+        {
+            InitializeComponent();
+
+            tscOptions.SelectedIndex = 0;
+        }
+
+        private void TscOptions_SelectedUpdate(object sender, EventArgs e)
+        {
+            switch (tscOptions.SelectedIndex)
+            {
+                case 1:
+                    tscG.Visible = true;
+                    tscE.Visible = true;
+                    tscL.Visible = true;
+
+                    tscCriteria.Visible = true;
+                    break;
+                case 3:
+                    tscML.Visible = true;
+                    tscCA.Visible = true;
+                    tscCL.Visible = true;
+                    tscCP.Visible = true;
+
+                    tscCriteria.Visible = false;
+                    break;
+                default:
+                    tscML.Visible = false;
+                    tscCA.Visible = false;
+                    tscCL.Visible = false;
+                    tscCP.Visible = false;
+
+                    tscG.Visible = false;
+                    tscE.Visible = false;
+                    tscL.Visible = false;
+
+                    tscCriteria.Visible = true;
+                    break;
+            }
+        }
+    }
+}
